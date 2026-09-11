0c21807006aee9a2d9795b07133786f658acddc8
Eric Tao <13306434+sperictao@users.noreply.github.com>
2026-09-11 22:44:24 +0800

    Load ProviderDialog layout fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "./style.css";
 import "./features/models/default-model-menu.css";
 import "./features/models/reasoning-effort.css";
 import "./features/models/advanced-settings.css";
+import "./features/models/provider-dialog.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
