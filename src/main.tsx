4651106cd26d76f50455a3ad1018ef974dbc6fa9
Eric Tao <13306434+sperictao@users.noreply.github.com>
2026-09-16 16:46:41 +0800

    ui: load Plugins secondary navigation styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import "./style.css";
 import "./page-layout.css";
+import "./features/market/market-navigation.css";
 import "./features/models/default-model-menu.css";
 import "./features/models/reasoning-effort.css";
 import "./features/models/advanced-settings.css";
